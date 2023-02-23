4ea28aefc0c41e7792da67b89b79db3ea9066c77
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-02-22 21:55:10 -0300

    modified:   MyFirstClass.java

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -2,16 +2,30 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
 
-        final String COUNTRY = "Brazil";
-        Integer anoFabricacao = 2022;
-        Double idade = 10.5;
-        String myName = "thiago";
-        System.out.print(idade);
+        // final String COUNTRY = "Brazil";
+        // Integer anoFabricacao = 2022;
+        // Double idade = 10.5;
+        // String myName = "thiago";
+        // System.out.print(idade);
 
         // String primeiroNome = "Thiago";
         // String segundoNome = "Aguiar";
         // String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
         // System.out.print(nomeCompleto);
+        
+        // String concatenacao = "1"+1+("4"+(1+2));
+        // System.out.println(concatenacao);
+
+        // Double salario = 10.5;
+        // salario++;
+        // System.out.println(salario);
+
+        String numero1 = "3";
+        String numero2 = "4";
+
+        boolean resultado = numero1.equals(numero2);
+
+        System.out.println(resultado);
 
     }
 
